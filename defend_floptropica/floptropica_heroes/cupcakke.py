77fbd6e6428c8cfa9346e426f68601160aa770c8
BaronMH <baronmheinrich@gmail.com>
2026-01-07 03:36:37 -0600

    added playable character classes

---
+++ defend_floptropica/floptropica_heroes/cupcakke.py
@@ -0,0 +1,10 @@
+import base_character
+class Cupcakke(base_character.BaseCharacter):
+    def __init__(self, name="Dummy", imagePath=None, movement=1, health=1000, energy=1000, abilityMenu=...):
+        super().__init__(name, imagePath, movement, health, energy, abilityMenu):
+        self.name = "Cupcakke"
+        self.imagePath = "./../character_assets/player_cupakke.png"
+        self.movement = 3
+        self.health = 1000
+        self.energy = 1000
+        self.abilityMenu = {
