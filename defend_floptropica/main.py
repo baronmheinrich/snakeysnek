04f219417391290bf1d4d0238ef2b5e9a73dab73
BaronMH <baronmheinrich@gmail.com>
2026-01-07 03:37:33 -0600

    added a main (duh!) with temp import statements

---
+++ defend_floptropica/main.py
@@ -0,0 +1,11 @@
+import game_master
+import base_character
+import base_enemy
+import base_abilities
+import levels
+
+def main():
+    gm = game_master.GameMaster()
+    bm = base_enemy.BaseEnemy
+    cm = base_character.BaseCharacter
+    am = base_abilities.Abilities
