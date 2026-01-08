b2628c67bc404fd087982d8b4c11a47e6340fd0e
BaronMH <baronmheinrich@gmail.com>
2026-01-07 23:31:39 -0600

    added tiles and tilemap for level 1

---
+++ defend_floptropica/levels/floptropica_tiled.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="floptropica_tiles" tilewidth="64" tileheight="32" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="tiles/08_neutral.aseprite" width="64" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="tiles/01_polluted.aseprite" width="64" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="tiles/02_grass.aseprite" width="64" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="tiles/03_sand.aseprite" width="64" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="tiles/04_lava.aseprite" width="64" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="tiles/05_ice.aseprite" width="64" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="tiles/06_water.aseprite" width="64" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="tiles/07_rocks.aseprite" width="64" height="32"/>
+ </tile>
+</tileset>
---
+++ defend_floptropica/levels/tiles/01_polluted.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="01_polluted" tilewidth="32" tileheight="16" tilecount="4" columns="2">
+ <image source="01_polluted.aseprite" width="64" height="32"/>
+</tileset>
