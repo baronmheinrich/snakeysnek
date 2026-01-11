3bb05e9b322863de7cae4d4e9468f444b90566c6
BaronMH <baronmheinrich@gmail.com>
2026-01-11 17:02:04 -0600

    changed grid orientation

--- defend_floptropica/levels/tiles/tileset_png.tsx
+++ defend_floptropica/levels/tiles/tileset_png.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tileset_png" tilewidth="64" tileheight="32" tilecount="9" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
+ <grid orientation="isometric" width="1" height="1"/>
  <tile id="0">
   <image source="05_ice.png" width="64" height="32"/>
  </tile>
