a966537e4f6e26c0cd97100d25013b49df374bb8
BaronMH <baronmheinrich@gmail.com>
2026-01-11 19:41:17 -0600

    added globals for tile width and height, adjusted screen size, adjust tile rendering arithmetic

--- defend_floptropica/levels/level1/level0.py
+++ defend_floptropica/levels/level1/level0.py
@@ -1,35 +1,38 @@
 import pygame, sys, os
 from pytmx.util_pygame import load_pygame
+import pytmx
+
+# Tile width is 64px by 32px!
+# It seems silly to put the divide by 2 but it's just for sanity reasons
+TILE_HEIGHT_HALF = 64/2
+TILE_WIDTH_HALF = 32/2
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
-        self.rect = self.image.get_rect(topright=pos)
+        self.rect = self.image.get_rect(topleft=pos)
 
-#Tile width is 64px!
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((860, 460))
 
+s_width = screen.get_width() / 2
+s_height = screen.get_height() / 8
 
 tmx_data = load_pygame("defend_floptropica/levels/tiles/tilemap_level1_v2.tmx")
 
-healthy_layer = tmx_data.get_layer_by_name('healthy')
-polluted_layer = tmx_data.get_layer_by_name('polluted')
+def draw_tiles(screen, layer_name, tmx_data):
+    layer = tmx_data.get_layer_by_name(layer_name)
+    for x, y, image in layer.tiles():
+        iso_x = (x - y) * (TILE_HEIGHT_HALF) + s_width - 30
+        iso_y = (x + y) * (TILE_WIDTH_HALF) + s_height + 20
+        screen.blit(image, (iso_x, iso_y))
 
-print("healthy layer:")
-for layer in healthy_layer:
-    print(layer)
-
-print("polluted layer:")
-for layer in polluted_layer:
-    print(layer)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
-    screen.fill('black')
+    draw_tiles(screen, "healthy", tmx_data)    
     pygame.display.update()
