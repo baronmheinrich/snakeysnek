7dca1689b38f90d0f7803aee74d36fe7116feb01
BaronMH <baronmheinrich@gmail.com>
2026-01-14 01:38:31 -0600

    added a cupcakke class and drew in  cupcakke's standee

--- defend_floptropica/levels/level1/level0.py
+++ defend_floptropica/levels/level1/level0.py
@@ -8,12 +8,26 @@ TILE_WIDTH_HALF = 32/2
 GRID_WIDTH = 860
 GRID_HEIGHT = 460
 
+class cupcakke(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        cupcakke_image = pygame.image.load(os.path.join("defend_floptropica/character_assets/standees/player_cupcakke.png")).convert_alpha()
+        cupcakke_image = pygame.transform.scale(cupcakke_image, (64, 128))
+        self.image = cupcakke_image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos_x, pos_y)
+
 
 pygame.init()
 screen = pygame.display.set_mode((GRID_WIDTH, GRID_HEIGHT))
 
 s_width = screen.get_width() / 2
 s_height = screen.get_height() / 8
+
+# I REALLY want to see if it's probable to use the array, or csv, or some data structure to keep track of the tiles.
+# Using the coordinates ALL THE TIME is going to be so laboroius.
+# Perhaps I can make a dictionary that keeps track of the tile coordinates and their types from the tmx_data object while drawing the map for the first time?
+cupcakke_player = cupcakke(367, 155)
 
 tmx_data = load_pygame("defend_floptropica/levels/tiles/tilemap_level1_v2.tmx")
 
@@ -23,6 +37,7 @@ def draw_tiles(screen, layer_name, tmx_data):
         iso_x = (x - y) * (TILE_HEIGHT_HALF) + s_width - 30
         iso_y = (x + y) * (TILE_WIDTH_HALF) + s_height + 20
         screen.blit(image, (iso_x, iso_y))
+
 
 # I think I might need to have a highlight tile..... unless I can draw an isometric square and ALSO layer it ontop of the map.
 # ToDo: Add tile highlight on mouseover
@@ -35,7 +50,11 @@ while True:
             pygame.quit()
             sys.exit()
 
-    draw_tiles(screen, "healthy", tmx_data)
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
+    draw_tiles(screen, "healthy", tmx_data)
+    
+    screen.blit(cupcakke_player.image, cupcakke_player.rect)
+    # screen.blit(cupcakke_image, (mouse_x, mouse_y))
+    
     pygame.display.update()
